351740075b775ed8e1a9bbb5d931e1af1bec3efb
Chen <litchian.chen@freightra.com>
2019-04-16 16:55:37 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+
+import { BackgroundGeolocation } from "@ionic-native/background-geolocation";
+import { HTTP } from "@ionic-native/http";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    BackgroundGeolocation,
+    HTTP,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+import {
+  BackgroundGeolocation,
+  BackgroundGeolocationConfig,
+  BackgroundGeolocationResponse,
+  BackgroundGeolocationEvents
+} from "@ionic-native/background-geolocation";
+import { HTTP } from "@ionic-native/http";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  gps_update_link: string = "your_http_request_link";
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private http: HTTP,
+    private backgroundGeolocation: BackgroundGeolocation
+  ) {}
 
+  startBackgroundGeolocation() {
+    const config: BackgroundGeolocationConfig = {
+      desiredAccuracy: 10,
+      stationaryRadius: 20,
+      distanceFilter: 30,
+      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+      stopOnTerminate: false // enable this to clear background location settings when the app terminates
+    };
+
+    this.backgroundGeolocation.configure(config).then(() => {
+      this.backgroundGeolocation
+        .on(BackgroundGeolocationEvents.location)
+        .subscribe((location: BackgroundGeolocationResponse) => {
+          console.log(location);
+          this.sendGPS(location);
+        });
+    });
+    // start recording location
+    this.backgroundGeolocation.start();
   }
 
+  sendGPS(location) {
+    if (location.speed == undefined) {
+      location.speed = 0;
+    }
+    let timestamp = new Date(location.time);
+    this.http
+      .post(
+        this.gps_update_link, // backend api to post
+        {
+          lat: location.latitude,
+          lng: location.longitude,
+          speed: location.speed,
+          timestamp: timestamp
+        },
+        {}
+      )
+      .then(data => {
+        console.log("POST Request is successful ", data);
+        this.backgroundGeolocation.finish(); // FOR IOS ONLY
+        // BackgroundGeolocation.endTask(taskKey);
+      })
+      .catch(error => {
+        this.backgroundGeolocation.finish(); // FOR IOS ONLY
+        console.log(error);
+      });
+  }
 }
